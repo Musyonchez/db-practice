5681ec9562ecf45f408f6177389cf6870f9cba55
Musyoka Philip <87429036+Musyonchez@users.noreply.github.com>
2023-11-17 10:18:39 +0300

    update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { sql } from "@vercel/postgres";
 
-const page = () => {
+export default async function Cart({ params }) {
+  const { rows } = await sql`SELECT * FROM CARTS WHERE user_id=${params.user}`.run({ POSTGRES_URL: process.env.POSTGRES_URL });
   return (
-    <div>page</div>
-  )
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default page
